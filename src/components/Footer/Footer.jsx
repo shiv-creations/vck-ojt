a498249073893051b2249d0fa037ff113e05e00d
shiv-creations <shivcreations40@gmail.com>
2025-06-11 09:13:56 +0530

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import './Footer.css';
-
+// src/components/Footer/Footer.jsx
+import './Footer.css'; // Import the CSS for the Footer
 const Footer = () => {
 return (
     <footer className="footer">
-        <div className="footer-content">
-            <p>&copy; {new Date().getFullYear()} My College. All rights reserved.</p>
-        </div>
+        <p>&copy; {new Date().getFullYear()} Vivekanand College. All rights reserved.</p>
     </footer>
 );
-};
-
+}
 export default Footer;
