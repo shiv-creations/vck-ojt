3668a6e02a9982492687e7bd63927282ce3e31e9
shiv-creations <shivcreations40@gmail.com>
2025-06-13 00:13:04 +0530

    second commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
       {/* Hero/Banner Section with Image */}
       <div className="hero-section">
         <img
-          src="/images/college-banner.png"
+          src="/images/college-banner.jpg"
           alt="Vivekanand College Campus"
           className="hero-banner-image"
         />
